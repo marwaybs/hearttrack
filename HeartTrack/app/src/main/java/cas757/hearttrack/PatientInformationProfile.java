d372574e6da7537c900fcf67cb9277f06ff14766
Bhaag Marway <bsmarway@gmail.com>
2017-02-24 11:29:18 -0500

    Started patient information profile UI

---
+++ HeartTrack/app/src/main/java/cas757/hearttrack/PatientInformationProfile.java
@@ -0,0 +1,13 @@
+package cas757.hearttrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientInformationProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_information_profile);
+    }
+}
